068ea3dde41d39ee5586c1a06498f2af8ff856a7
G4BVEI <gabdaveiga@gmail.com>
2026-02-01 12:05:55 -0300

    idk

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -5,8 +5,22 @@
     git
     gh
     nh
-    yazi
     fastfetch
     mapscii
+    starship
+    environment.systemPackages = with pkgs; [
+	(yazi.override {
+		_7zz = _7zz-rar;  # Support for RAR extraction
+	})
+    ];
+    # archives
+    p7zip
+    # search / navigation
+    fd
+    ripgrep
+    fzf
+    zoxide
+    # misc
+    jq
   ];
 }
