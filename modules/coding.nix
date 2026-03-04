9dca9b6d847b6c0ccceabd5ad7856865ddcfc0cd
G4BVEI <gabdaveiga@gmail.com>
2026-03-03 21:42:10 -0300

    dirty git tree

--- modules/coding.nix
+++ modules/coding.nix
@@ -7,14 +7,13 @@
     #lsp
     nixd
     nil
-    qt6.qtdeclarative
+    devenv
   ];
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     nixd
     nil
   ];
-
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true; # important for nix shells
 }
