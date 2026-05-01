e824caf1389eef4ae1e987dcc0099eb3fcc13e04
G4BVEI <gabdaveiga@gmail.com>
2026-05-01 12:02:54 -0300

    i trully hate flutter btw

--- devshells/flutter-devenv/devenv.nix
+++ devshells/flutter-devenv/devenv.nix
@@ -11,10 +11,4 @@
     enable = true;
     flutter.enable = true;
   };
-  packages = [
-    pkgs.chromium
-  ];
-  enterShell = ''
-    export CHROME_EXECUTABLE=${pkgs.chromium}/bin/chromium
-  '';
 }
