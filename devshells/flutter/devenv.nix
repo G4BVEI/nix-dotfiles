8f1eca5efb8c03cf77986d7b9d462eb172c5bf48
G4BVEI <gabdaveiga@gmail.com>
2026-05-29 22:16:55 -0300

    quickfix

--- devshells/flutter/devenv.nix
+++ devshells/flutter/devenv.nix
@@ -7,12 +7,16 @@
 }:
 
 {
+  packages = [
+    pkgs.sqlite
+  ];
   android = {
     enable = true;
     flutter.enable = true;
   };
   enterShell = ''
     export CHROME_EXECUTABLE=${pkgs.chromium}/bin/chromium
+    export LD_LIBRARY_PATH=${pkgs.sqlite.out}/lib:$LD_LIBRARY_PATH
     source <(flutter bash-completion)
   '';
 }
